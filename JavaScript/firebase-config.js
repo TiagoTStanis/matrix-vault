0e8720b8f259bde03bfc59768ab1596f688558f0
TIAGO TORRES STANIS <70165676+TiagoTStanis@users.noreply.github.com>
2024-10-24 08:20:49 -0300

    Alterações em algumas seguranças

--- JavaScript/firebase-config.js
+++ JavaScript/firebase-config.js
@@ -2,13 +2,7 @@ import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/11.0.
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
 const firebaseConfig = {
-    apiKey: "AIzaSyBz3o1YgacJNA7rJeWhUX7qACEcph0xj6c",
-    authDomain: "devportifolio-ff745.firebaseapp.com",
-    projectId: "devportifolio-ff745",
-    storageBucket: "devportifolio-ff745.appspot.com",
-    messagingSenderId: "935025773642",
-    appId: "1:935025773642:web:0911bafd72ceb3307beab3",
-    measurementId: "G-8YVNRD1XHH"
+
 };
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 const auth = getAuth(app);
